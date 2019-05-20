b4eb604f97fb0b1018a19bc061e70387d17a6c16
Tueloper <ozurumbatochukwu@yahoo.com>
2019-05-20 11:39:42 +0100

    Event states for Button

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,14 +2,20 @@
 const socket = io()
 const increment = document.getElementById('increment')
 const display = document.getElementById('display')
-const send = document.getElementById('send')
+const messageForm = document.getElementById('messageForm')
 const getLocation = document.getElementById('location')
-
+const messageInput = document.getElementById('messageInput')
+const messageButton = document.getElementById('messageButton')
+ 
 socket.on('sendMessage', (inputValue) => {
     console.log(inputValue)
 })
 
 getLocation.addEventListener('click', () => {
+
+    //disable location button
+    getLocation.setAttribute( 'disabled', 'disabled')
+
     if ( !navigator.geolocation ) {
         return alert('Geolocation is not Supported by your Browser')
     }
@@ -21,6 +27,8 @@ getLocation.addEventListener('click', () => {
           longitude: position.coords.longitude
       }, () => {
 
+        //enable the location button
+        getLocation.removeAttribute('disabled')
         //acknowledgement function
         console.log('Location Shared')
       })
@@ -28,13 +36,21 @@ getLocation.addEventListener('click', () => {
 })
 
 //this is for sending messages 
-send.addEventListener('submit', (e) => {
+messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
     // console.log('Submitted')
     
+    //disable the message button to make sure the message is sent before another message is sent
+    messageButton.setAttribute('disabled', 'disabled')
+
     const inputValue = e.target.elements.messageInput.value
     socket.emit( 'message', inputValue, (error) => {
         //this is an acknowledgement event
+
+        //enabling the button after inputValue is sent
+        messageButton.removeAttribute('disabled')
+        messageInput.value = ''
+        messageInput.focus()
 
         if (error) {
             return console.log(error)
